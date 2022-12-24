30635ccd190a7dc25c889a40f2794d833af6279a
Santiago de Pedro <santiago.pedro23@gmail.com>
2022-12-24 18:04:42 -0400

    Refactoring
    Level up the version of the library and remove CI configuration.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject atmos-web-kernel-reitit "1.0-SNAPSHOT"
+(defproject atmos-web-kernel-reitit "2.0-SNAPSHOT"
   :description "This library is the web core of atmos systems, it has commons functionality using *Reitit*."
   :url "https://github.com/AtmosSystem/atmos-web-kernel-reitit"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [atmos-kernel "2.2"]
+                 [atmos-kernel "2.2-SNAPSHOT"]
                  [metosin/spec-tools "0.10.5"]
                  [javax.servlet/javax.servlet-api "4.0.1"]
                  ; Ring
