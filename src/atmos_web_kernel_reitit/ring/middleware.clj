c6ec0d7feeb05bb38b71e46f53b567f5c40d5cb1
Santiago de Pedro <santiago.pedro23@gmail.com>
2023-10-26 10:34:37 -0400

    Ring: CORS
    Adding cors middleware

--- src/atmos_web_kernel_reitit/ring/middleware.clj
+++ src/atmos_web_kernel_reitit/ring/middleware.clj
@@ -1,8 +1,15 @@
 (ns atmos-web-kernel-reitit.ring.middleware
-  (:require [atmos-web-kernel-reitit.core :as c]
-            [clojure.spec.alpha :as s]))
+  (:require
+    [atmos-kernel.configuration :as config]
+    [atmos-web-kernel-reitit.core :as c]
+    [clojure.spec.alpha :as s]
+    [ring.middleware.cors :refer [wrap-cors]]
+    [clojure.string :as string]))
+
+(def settings (config/read-edn :settings))
 
 (s/def ::status-with-response (s/map-of pos-int? any? :count 1))
+
 
 (def web-response-middleware {:name        ::web-response
                               :description "Middleware to add a ring-valid response"
@@ -12,3 +19,12 @@
                                                      [http-status data] (if (s/valid? ::status-with-response response)
                                                                           (into [] (first response)) [200 response])]
                                                  (c/web-response data (keyword (str http-status))))))})
+
+(def web-cors-middleware {:name        ::web-cors
+                          :description "Middleware to add CORS functionality"
+                          :wrap        (fn [ring-handler]
+                                         (let [origins (map re-pattern (string/split (get-in settings [:cors :origins] "") #","))
+                                               methods (get-in settings [:cors :methods] [])]
+                                           (wrap-cors ring-handler
+                                                      :access-control-allow-origin origins
+                                                      :access-control-allow-methods methods)))})
